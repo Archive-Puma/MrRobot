a07618e6e36034b341704e64bd23b408388ba960
Puma on the Ubuntu <kikefontanlorenzo@gmail.com>
2019-12-22 04:10:46 +0100

    :speech_balloon: Some little changes in the arguments
    :lipstick: Fixed comments identation
    :zap: Public Arguments type

--- src/environment/arguments.rs
+++ src/environment/arguments.rs
@@ -1,26 +1,27 @@
+// external crates (argument parser
 extern crate clap;
-
-use clap::{App,Arg,AppSettings,ArgMatches};
+use clap::{App,Arg,AppSettings};
+pub use clap::ArgMatches as Arguments;
 
 pub fn get
-() -> ArgMatches <'static>
+() -> Arguments <'static>
 {
 	return App::new("Mr.Robot")
 		.version("1.0.0")
 		.author("Kike Fontán (@CosasDePuma) <kikefontanlorenzo@gmail.com>")
 		.about("A robot to automate the hacking process")
 
-		.setting(AppSettings::ArgRequiredElseHelp)		// shows help if not arguments provided
-		.setting(AppSettings::ColorNever)				// shows the help without colors
-		.setting(AppSettings::NextLineHelp)				// places the help string on the line after
-		.setting(AppSettings::UnifiedHelpMessage)		// joins FLAGS and OPTIONS in a single group
+		.setting(AppSettings::ArgRequiredElseHelp)  // shows help if not arguments provided
+		.setting(AppSettings::ColorNever)           // shows the help without colors
+		.setting(AppSettings::NextLineHelp)         // places the help string on the line after
+		.setting(AppSettings::UnifiedHelpMessage)   // joins FLAGS and OPTIONS in a single group
 
-		.arg(Arg::with_name("file")
+		.arg(Arg::with_name("composer")
 				.short("f")
 				.long("file")
 				.required(true)
 				.takes_value(true)
-				.help("Specifies the compose file"))
+				.help("Specifies the composer"))
 		.get_matches();
 }
 
