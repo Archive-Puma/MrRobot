80775bed2987baf41255c3fcc82b561d227466ad
Puma on the Ubuntu <kikefontanlorenzo@gmail.com>
2019-12-22 04:15:34 +0100

    :rocket: Introducing the root!
    :sparkles: Entrypoint function with the dataflow
    :sparkles: Error handler in the main function

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+// Implement the mods
+mod composer;
+mod environment;
+
+// Bind the real names for the mods;
+use composer::reader::{read,Yaml};
+use composer::validator::validate;
+use environment::arguments;
+use environment::arguments::Arguments;
+use environment::mrerror::Result;
+
+
+fn main
+() -> ()
+{
+	match entrypoint()
+	{
+		Ok(_)    => println!("[+] All done!"),
+		Err(err) => println!("[!] {}.", err.kind.as_str())
+	}
+}
+
+fn entrypoint
+() -> Result<()>
+{
+	// Get the arguments
+	let arguments: Arguments = arguments::get();
+	let composer: &str = arguments.value_of("composer").unwrap();
+	// Parse the composer
+	let data: &Yaml = &read(composer)?;
+	validate(data)?;
+	
+	Ok(())
+}
+
