04f5751a7c527b5badb32d13b97f2237c4e77541
Pumita <kikefontanlorenzo@gmail.com>
2019-12-30 17:30:13 +0100

    :bug: Fixing Regex captures

--- src/composer/regexp.rs
+++ src/composer/regexp.rs
@@ -1,10 +1,9 @@
-extern crate regex;
 use regex::Regex;
 
 pub fn get_variable_name(text: &str) -> Option<String> {
-    let re: Regex = Regex::new(r"^\{\{([^\}])\}\}$").unwrap();
-    if re.is_match(text) {
-        let input: &str = re.captures(text).unwrap().get(0).unwrap().as_str().trim();
-        Some(input.to_string())
-    } else { None }
+    let re: Regex = Regex::new(r"^\$\{\{([^\}]+)\}\}$").unwrap();
+    match re.captures(text) {
+        None => None,
+        Some(captures) => Some(captures[1].trim().to_string())
+    }
 }
